10fa74b03866afc2cf952487614937b225afdb0e
elhamkariminia <e.kariminia63@gmail.com>
2022-05-09 19:52:54 +0430

    add block row

--- js/app.js
+++ js/app.js
@@ -1,1 +1,29 @@
-alert("salaam");
+var randomtass ;
+
+function btnOpenClick()
+{
+   var randomTass=Math.floor(Math.random()*6)+1;
+   var tassInput=document.getElementById("inputid");
+   tassInput.setAttribute('value', randomTass );
+   console.log(tassInput);
+   
+}
+function inputname()
+{
+   function setInputData(inputId){
+      var player = prompt("Please enter your name:" );
+      var playername=document.getElementById(inputId);
+      playername.setAttribute('value',player);
+   }
+
+   setInputData("playerid");
+
+
+   var player = prompt("Please enter your name:" );
+   var playername=document.getElementById("player2id");
+   playername.setAttribute('value',player);
+}
+function row()
+{
+   
+}
