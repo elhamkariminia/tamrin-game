50ae7a0451d794a720ffa298fb86e00567b9d970
elham kariminia <1992@fanrp-infra.local>
2022-08-13 17:19:08 +0430

    harkat mohre 1 dar box bar asase randomtass

--- js/app.js
+++ js/app.js
@@ -1,29 +1,45 @@
-var randomtass ;
+var randomTass;
+var dataBoxNumber;
+var newplayerlocation;
+var pleyer1place = 0;
 
-function btnOpenClick()
-{
-   var randomTass=Math.floor(Math.random()*6)+1;
-   var tassInput=document.getElementById("inputid");
-   tassInput.setAttribute('value', randomTass );
-   console.log(tassInput);
-   
+function randomTassFn() {
+  randomTass = Math.floor(Math.random() * 6) + 1;
+  var tassInput = document.getElementById("inputid");
+  tassInput.setAttribute("value", randomTass);
 }
-function inputname()
-{
-   function setInputData(inputId){
-      var player = prompt("Please enter your name:" );
-      var playername=document.getElementById(inputId);
-      playername.setAttribute('value',player);
-   }
 
-   setInputData("playerid");
+// function inputname() {
+//   function setInputData(inputId) {
+//     var player = prompt("Please enter your name:");
+//     var playername = document.getElementById(inputId);
+//     playername.setAttribute("value", player);
+//   }
 
+//   setInputData("playerid");
 
-   var player = prompt("Please enter your name:" );
-   var playername=document.getElementById("player2id");
-   playername.setAttribute('value',player);
+//   var player = prompt("Please enter your name:");
+//   var playername = document.getElementById("player2id");
+//   playername.setAttribute("value", player);
+// }
+function testFn(){
+ var tt= returnMohreP1Placement();
+ alert(tt);
 }
-function row()
-{
-   
+
+function returnMohreP1Placement(){
+  var PN=null; //PN=Placement number
+  var player = document.getElementById("mohrep1");
+  PN=player.closest('.box');
+  placeP1=PN.getAttribute("data-box-number");
+
+  return parseInt(placeP1);
 }
+
+function movement(event) {
+  var databoxNumber =  returnMohreP1Placement() + parseInt(randomTass);
+  var player = document.getElementById("mohrep1");
+  var mybox = document.querySelector("div.box[data-box-number='" + parseInt(databoxNumber) + "'] .p1id");
+  mybox.appendChild(player);
+}
+
