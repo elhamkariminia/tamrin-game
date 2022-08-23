78503c50c5c1897895f163a300db3b2b45c4bf84
elham kariminia <1992@fanrp-infra.local>
2022-08-23 08:14:00 +0430

    function tass va otomat kardane kelid move

--- js/app.js
+++ js/app.js
@@ -3,28 +3,38 @@ var dataBoxNumber;
 var newplayerlocation;
 var pleyer1place = 0;
 
-function randomTassFn() {
-  randomTass = Math.floor(Math.random() * 6) + 1;
-  var tassInput = document.getElementById("inputid");
-  tassInput.setAttribute("value", randomTass);
+//function randomTassFn() {
+ // randomTass = Math.floor(Math.random() * 6) + 1;
+ // return randomTass;
+//}
+//unction tassInput() {
+ // var tassInput = document.getElementById("inputid");
+  //return tassInput;
+//}
+
+//function setTassInputValue(tassInput,randomTass){
+ 
+  //tassInput().setAttribute("value", randomTassFn());
+//}
+function tass(){
+  randomTass= Math.floor(Math.random() * 6) + 1;
+  return randomTass;
 }
 
-// function inputname() {
-//   function setInputData(inputId) {
-//     var player = prompt("Please enter your name:");
-//     var playername = document.getElementById(inputId);
-//     playername.setAttribute("value", player);
-//   }
+function setInputTassValue(value){
+ var inputTass= document.getElementById("inputid");
+ inputTass.setAttribute("value", value);
+}
 
-//   setInputData("playerid");
+function x(){
+  var tt= tass();
+  setInputTassValue(tt);
+  movement();
+}
 
-//   var player = prompt("Please enter your name:");
-//   var playername = document.getElementById("player2id");
-//   playername.setAttribute("value", player);
-// }
 function testFn(){
- var tt= returnMohreP1Placement();
- alert(tt);
+ var tt= setInputTassValue("5");
+//  alert(tt);
 }
 
 function returnMohreP1Placement(){
@@ -36,14 +46,14 @@ function returnMohreP1Placement(){
 }
 
 function clearRandomTass(){
-  randomTass=" ";
+  setInputTassValue(null);
 }
 
 function movement(event) {
   var databoxNumber =  returnMohreP1Placement() + parseInt(randomTass);
   var player = document.getElementById("mohrep1");
   var mybox = document.querySelector("div.box[data-box-number='" + parseInt(databoxNumber) + "'] .p1id");
-  clearRandomTass();
+  //clearRandomTass();
   if (mybox<20){
     alert("next player");
   }
